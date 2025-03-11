c77da03fdaa9f931a158c5627919d7c7a23d2d6e
wxxFE <2022095028@qq.com>
2025-03-11 23:57:08 +0800

    移除多余代码

--- public/beautiful/assets/导航条切换滑动选中效果/script.js
+++
@@ -1,7 +0,0 @@
-/* udpate active button */
-document.querySelector("nav").addEventListener("click", (e) => {
-  if (e.target.tagName == "BUTTON") {
-    document.querySelector("nav .active").classList.remove("active");
-    e.target.classList.add("active");
-  }
-});
